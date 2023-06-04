bedd60b8df1735e93e4c195ec86a87bba61cf9c5
NadaHSalem <121772796+NadaHSalem@users.noreply.github.com>
2023-06-04 18:48:42 -0400

    Fixed issue with the App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import gmail from "./assets/gmail.png";
+import linkedin from "./assets/linkedin.png";
+import github from "./assets/git.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="web-padding">
+      <p className="h1-style">Hi!</p>
+      <p className="h2-style"> Welcome to my website :{")"}</p>
+      <p className="home-body-style">
+        I'm a Mechatronics Student at the University of Waterloo in my 1B co-op
+        term. This website features some of my code, mechanical design projects,
+        and art!
+      </p>
+      <p className="home-body-style"> I hope you have fun exploring.</p>
+      <a href="https://www.linkedin.com/in/nada-h-salem/" target="_blank">
+        <img src={linkedin} alt="LinkedIn Icon" className="icon-style" />
+      </a>
+      <a href="https://github.com/NadaHSalem" target="_blank">
+        <img src={github} alt="GitHhub Icon" className="icon-style invert" />
+      </a>
+      <a href="https://github.com/NadaHSalem" target="_blank">
+        <img src={gmail} alt="Gmail Icon" className="icon-style" />
+      </a>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "./fonts/MatSaleh.otf";
+import "./index.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Code from "./routes/Code";
+import Art3D from "./routes/Art3D";
+import NavBar from "./components/NavBar";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<App />}></Route>
+          <Route path="/home" element={<App />}></Route>
+          <Route path="/code" element={<Code />}></Route>
+          <Route path="/art&3d" element={<Art3D />}></Route>
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
